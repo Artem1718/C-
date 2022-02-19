e4606d3ecad6b99b27bce2811f1d5a115f181b2a
Artem1718 <85923409+Artem1718@users.noreply.github.com>
2022-02-19 10:42:11 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void funcshion(int x)
+        {
+            int r = 0;
+            while (x != 0)
+            {
+                double a = x % 10;
+                x /= 10;
+                double s = x % 10;
+                if (s < a) { r = 1; } else { r = 0; }
+                if (r == 0) { break; }
+            }
+            if (r == 1) { Console.WriteLine("Yes"); } else { Console.WriteLine("No"); }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int x = int.Parse(Console.ReadLine());
+            funcshion(x);
+            Console.ReadLine();
+        }
+    }
+}
